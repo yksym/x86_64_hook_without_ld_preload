57cff5341137370e0dd0ddca9bd6b0b0063b1033
yksym <yksym@google.com>
2015-08-02 20:00:03 +0900

    use objdump instead of ltrace(gelf)

--- src/hook.c
+++ src/hook.c
@@ -1,43 +1,76 @@
 #define _GNU_SOURCE
 #include <stdio.h>
+#include <stdlib.h>
 #include <stdint.h>
 #include <string.h>
 #include <link.h>
-#include <gelf.h>
+//#include <gelf.h>
 #include "hook.h"
 
 struct Arg {
-    const char* libPath;//in
+    const char* libname;//in
     size_t offset;//out
+    char libpath[512];
 };
 
 static int cb(struct dl_phdr_info *info, size_t size, void* data)
 {
     struct Arg* arg = (struct Arg*) data;
+    const char* hoge = basename(info->dlpi_name);
     //printf("%08lx %s\n", info->dlpi_addr, info->dlpi_name);
-    if (strcmp(arg->libPath, info->dlpi_name) == 0) {
+    if (strcmp(basename(arg->libname), basename(info->dlpi_name)) == 0) {
         arg->offset = (size_t) info->dlpi_addr;
+        strcpy(arg->libpath, info->dlpi_name);
     }
     return 0;
 }
 
-void* getPLTAddrFromElf(const char *filename, const char* funcname, GElf_Addr bias);
-
-static Addr_t getPLTAddr(const char* libPath, const char* funcname)
+int execCmd(const char* cmd, char* output)
 {
-    struct Arg arg;
-    arg.libPath = libPath;
-    arg.offset = 0;
-    dl_iterate_phdr(cb, &arg);
-
-    size_t addr = (size_t)getPLTAddrFromElf(libPath, funcname, arg.offset);
-
-    return (Addr_t)(addr + arg.offset);
+    FILE    *fp;
+    //puts(cmd);
+    if ( (fp=popen(cmd, "r")) ==NULL) {
+        return -1;
+    }
+    while(fgets(output, 1024, fp) != NULL) {
+    }
+    (void) pclose(fp);
+    return 0;
 }
 
-Addr_t getGOTAddr(const char* libPath, const char* funcname)
+
+void* getPLTAddrFromElf(const char *filepath, const char* funcname, size_t bias)
 {
-    Addr_t plt = getPLTAddr(libPath, funcname);
+    char cmd[512];
+    char output[512];
+    sprintf(cmd, "objdump -d %s | grep '<%s@plt>:' | awk '{print $1}' | head -n 1", filepath, funcname);
+    int ret = execCmd(cmd, output);
+    if (ret) {
+        exit (-1);
+    }
+    //puts(output);
+    long n = strtol(output, NULL, 16);
+    return (void*)(n + bias);
+}
+
+
+static Addr_t getPLTAddr(const char* libname, const char* funcname)
+{
+    struct Arg arg;
+    arg.libname = libname;
+    arg.offset = 0;
+    strcpy(arg.libpath, libname);
+    dl_iterate_phdr(cb, &arg);
+
+    void* addr = getPLTAddrFromElf(arg.libpath, funcname, arg.offset);
+    //printf("%s %s %p\n", arg.libpath, funcname, addr);
+
+    return (Addr_t)(addr);
+}
+
+Addr_t getGOTAddr(const char* libname, const char* funcname)
+{
+    Addr_t plt = getPLTAddr(libname, funcname);
     //printf("%p\n", plt);
     uint8_t jmp[6];
     memcpy(jmp, plt, sizeof(jmp));
--- test/test2.cpp
+++ test/test2.cpp
@@ -4,19 +4,19 @@
 
 
 extern "C" {
-    void* calloc_hook(size_t n, size_t size)
+    void* calloc_hook2(size_t n, size_t size)
     {
         DECL_ORG_FUNC(calloc, org_calloc);
-        puts( __FUNCTION__ );
+        puts(__FUNCTION__ );
         return org_calloc(n, size);
     }
 };
 
-void test(const char* program)
+void test2(const char* program)
 {
     puts(__FILE__ ": 1st");
     {
-        HOOK(program, calloc, calloc_hook);
+        HOOK(program, calloc, calloc_hook2);
         void* b = calloc(1, 10);
         free(b);
     }
@@ -27,17 +27,10 @@ void test(const char* program)
 
     puts(__FILE__ ": 3rd");
     {
-        HOOK(program, calloc, calloc_hook);
+        HOOK(program, calloc, calloc_hook2);
         void* b = calloc(1, 10);
         free(b);
     }
 }
 
-void test2(const char* program);
 
-int main(int argc, char** argv)
-{
-    test(argv[0]);
-    test2(LIB_NAME);
-    return 0;
-}
